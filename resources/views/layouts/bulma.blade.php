0c5cb006e8e3e9996603ada481893f1defe1b28e
Fr4nzJ <ermido09a@gmail.com>
2026-02-23 13:56:59 +0800

    Fix navbar logo case sensitivity for Linux deployment
    - Changed 'GAD logo.png' to 'GAD Logo.png' to match actual filename
    - Windows is case-insensitive, but Railway (Linux) is case-sensitive
    - This fixes broken navbar logo on production deployment

--- resources/views/layouts/bulma.blade.php
+++ resources/views/layouts/bulma.blade.php
@@ -339,7 +339,7 @@
         <div class="navbar-brand">
             <a class="navbar-item" href="{{ route('welcome') }}">
                 <span class="icon-text">
-                    <img src="{{ asset('images/GAD logo.png')}}" alt="CatSu GAD Logo" style="height: 40px; margin-right: 10px;">
+                    <img src="{{ asset('images/GAD Logo.png')}}" alt="CatSu GAD Logo" style="height: 40px; margin-right: 10px;">
                     <span style="color: #667eea; font-weight: bold;">CatSu GAD</span>
                 </span>
             </a>
